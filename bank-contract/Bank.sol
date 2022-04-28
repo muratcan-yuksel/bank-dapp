8a81a8a993e2d5d2d49fff4e938f53bb38265893
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-29 00:55:53 +0300

    contract is done!

--- bank-contract/Bank.sol
+++ bank-contract/Bank.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 contract Bank{
     address public BankOwner;
     string public bankName;
+    uint withdrawTime;
     mapping (address => uint256) public customerBalance;
 
 constructor(){
@@ -24,6 +25,7 @@ modifier fundsRequired(uint256 _total){
     _;
 }
 
+
 function depositMoney() public payable valueRequired {
     customerBalance[msg.sender] += msg.value;
 }
@@ -31,9 +33,13 @@ function depositMoney() public payable valueRequired {
 function setBankName(string memory _name) external ownerRequired {
     bankName= _name;
 }
-function withdrawMoney(address payable _to, uint256 _total) public fundsRequired(_total) {
+function withdrawMoney(address payable _to, uint256 _total) public payable fundsRequired(_total) {
     customerBalance[msg.sender] -= _total;
     _to.transfer(_total);
+//seems 2 modifiers in 1 function doesn't work
+//so I put a require here
+     require(block.timestamp >= (withdrawTime + 30 seconds), "You can withdraw only once per 30 seconds");
+    withdrawTime= block.timestamp;
 }
 
 function getCustomerBalance() external view returns(uint256){
