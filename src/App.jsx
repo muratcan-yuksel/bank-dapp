ac1e7ba86be6fd992bda44f326767f894e343293
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-02 18:03:36 +0300

    fallback

--- src/App.jsx
+++ src/App.jsx
@@ -194,19 +194,112 @@ const App = () => {
     }
   };
 
+  const handleInputChange = (event) => {
+    setInputValue((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
     getBankName();
-    getbankOwnerHandler();
-    customerBalanceHanlder();
+    getBankOwnerHandler();
+    customerBalanceHandler();
   }, [isWalletConnected]);
 
   return (
     <div className="App">
-      App Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis
-      necessitatibus voluptas, praesentium est commodi, mollitia sapiente
-      doloremque iste unde odit, recusandae suscipit laboriosam aperiam
-      consequatur ipsa beatae maiores non autem.
+      <main className="main-container">
+        <h2 className="headline">
+          <span className="headline-gradient">Bank Contract Project</span> 💰
+        </h2>
+        <section className="customer-section px-10 pt-5 pb-10">
+          {error && <p className="text-2xl text-red-700">{error}</p>}
+          <div className="mt-5">
+            {currentBankName === "" && isBankerOwner ? (
+              <p>"Setup the name of your bank." </p>
+            ) : (
+              <p className="text-3xl font-bold">{currentBankName}</p>
+            )}
+          </div>
+          <div className="mt-7 mb-9">
+            <form className="form-style">
+              <input
+                type="text"
+                className="input-style"
+                onChange={handleInputChange}
+                name="deposit"
+                placeholder="0.0000 ETH"
+                value={inputValue.deposit}
+              />
+              <button className="btn-purple" onClick={depositMoneyHandler}>
+                Deposit Money In ETH
+              </button>
+            </form>
+          </div>
+          <div className="mt-10 mb-10">
+            <form className="form-style">
+              <input
+                type="text"
+                className="input-style"
+                onChange={handleInputChange}
+                name="withdraw"
+                placeholder="0.0000 ETH"
+                value={inputValue.withdraw}
+              />
+              <button className="btn-purple" onClick={withDrawMoneyHandler}>
+                Withdraw Money In ETH
+              </button>
+            </form>
+          </div>
+          <div className="mt-5">
+            <p>
+              <span className="font-bold">Customer Balance: </span>
+              {customerTotalBalance}
+            </p>
+          </div>
+          <div className="mt-5">
+            <p>
+              <span className="font-bold">Bank Owner Address: </span>
+              {bankOwnerAddress}
+            </p>
+          </div>
+          <div className="mt-5">
+            {isWalletConnected && (
+              <p>
+                <span className="font-bold">Your Wallet Address: </span>
+                {customerAddress}
+              </p>
+            )}
+            <button className="btn-connect" onClick={checkIfWalletIsConnected}>
+              {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
+            </button>
+          </div>
+        </section>
+        {isBankerOwner && (
+          <section className="bank-owner-section">
+            <h2 className="text-xl border-b-2 border-indigo-500 px-10 py-4 font-bold">
+              Bank Admin Panel
+            </h2>
+            <div className="p-10">
+              <form className="form-style">
+                <input
+                  type="text"
+                  className="input-style"
+                  onChange={handleInputChange}
+                  name="bankName"
+                  placeholder="Enter a Name for Your Bank"
+                  value={inputValue.bankName}
+                />
+                <button className="btn-grey" onClick={setBankNameHandler}>
+                  Set Bank Name
+                </button>
+              </form>
+            </div>
+          </section>
+        )}
+      </main>
     </div>
   );
 };
